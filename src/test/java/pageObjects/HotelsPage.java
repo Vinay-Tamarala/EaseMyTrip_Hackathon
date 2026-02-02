1eb18158031808a8713c8d19f5cc9bd0b8f00d12
Yadav <2457422@cognizant.com>
2026-02-02 15:28:27 +0530

    Added testcases and merging them

--- src/test/java/pageObjects/HotelsPage.java
+++ src/test/java/pageObjects/HotelsPage.java
@@ -1,69 +1,101 @@
 package pageObjects;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import java.util.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+public class HotelsPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
 
-public class HotelsPage  {
-    WebDriver driver;
-    WebDriverWait wait;
-
-    public HotelsPage(WebDriver driver, WebDriverWait wait){
-        this.driver=driver;
-        PageFactory.initElements(driver,this);
+    public HotelsPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
     }
-    @FindBy(xpath = "//div[@class='htl_location shwbx']\"")
-    WebElement ClickSelectBox;
 
-    @FindBy(xpath = "//input[@id='txtCity']\"")
-    WebElement SelectPlace;
+    @FindBy(xpath = "//span[@class='meuicowidth hotelmenuico']")
+    private WebElement hotelsButton;
+
+    @FindBy(xpath = "//div[@class='htl_location shwbx']")
+    private WebElement clickSelectBox;
+
+    @FindBy(xpath = "//input[@id='txtCity']")
+    private WebElement selectPlace;
 
     @FindBy(xpath = "(//div[@class='hp_inputBox ht-dates'])[1]")
-    WebElement selectCheckIn;
+    private WebElement selectCheckIn;
+
     @FindBy(xpath = "//div[@class='ui-datepicker-group ui-datepicker-group-last']//table//tbody//a[text()='12']")
-    WebElement clickCheckIn;
+    private WebElement clickCheckIn;
+
     @FindBy(xpath = "(//div[@class='hp_inputBox ht-dates'])[2]")
-    WebElement selectCheckOut;
+    private WebElement selectCheckOut;
+
     @FindBy(xpath = "//div[@class='ui-datepicker-group ui-datepicker-group-last']//table//tbody//a[text()='14']")
-    WebElement clickCheckOut;
+    private WebElement clickCheckOut;
 
-    @FindBy(id="Adults_room_1_1_plus")
-    WebElement adult_plus_button;
+    @FindBy(id = "Adults_room_1_1_plus")
+    private WebElement adultPlusButton;
 
-    public void HotelAction() {
-        ClickSelectBox.click();
-        SelectPlace.sendKeys("Manali");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//ul[@id='divAppend']"))));
+    @FindBy(xpath = "//span[@id='Adults_room_1_1']")
+    private WebElement adultCountSpan;
+
+    public void navigateToHotels() {
+        wait.until(ExpectedConditions.elementToBeClickable(hotelsButton)).click();
+    }
+
+    public void hotelAction() {
+        // Location
+        wait.until(ExpectedConditions.elementToBeClickable(clickSelectBox)).click();
+        wait.until(ExpectedConditions.visibilityOf(selectPlace)).clear();
+        selectPlace.sendKeys("Manali");
+
+        // Wait for suggestions and pick first
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@id='divAppend']")));
         try {
             driver.findElement(By.xpath("(//div[@class='hflexaut'])[1]")).click();
-        }
-        catch(StaleElementReferenceException e)
-        {
+        } catch (StaleElementReferenceException e) {
+            // Retry once more in case the list refreshed
             driver.findElement(By.xpath("(//div[@class='hflexaut'])[1]")).click();
         }
-        selectCheckIn.click();
-        clickCheckIn.click();
-        selectCheckOut.click();
-        clickCheckOut.click();
-        while(adult_plus_button.isEnabled()){
-            int before=Integer.parseInt(driver.findElement(By.xpath("//span[@id='Adults_room_1_1']")).getText());
-            adult_plus_button.click();
-            int after=Integer.parseInt(driver.findElement(By.xpath("//span[@id='Adults_room_1_1']")).getText());
-            if(before==after)
+
+        // Dates
+        wait.until(ExpectedConditions.elementToBeClickable(selectCheckIn)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(clickCheckIn)).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(selectCheckOut)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(clickCheckOut)).click();
+    }
+
+    public void IncreamentAdult() {
+        // Increase adults until it no longer increments
+        while (adultPlusButton.isEnabled()) {
+            int before = Integer.parseInt(adultCountSpan.getText().trim());
+            adultPlusButton.click();
+            int after = Integer.parseInt(adultCountSpan.getText().trim());
+            if (before == after) {
                 break;
+            }
         }
     }
-    public void getMaxAdultCount(){
-        int x=Integer.parseInt(driver.findElement(By.xpath("//span[@id='Adults_room_1_1']")).getText());
-        System.out.println("Max adult count is:"+x);
+
+    public int getMaxAdultCount() {
+        int x = Integer.parseInt(adultCountSpan.getText().trim());
+        System.out.println("Max adult count is: " + x);
+        return x;
+    }
+
+    public String getCheckInDate() {
+        // Returns the day text that was clicked (e.g., "12")
+        return clickCheckIn.getText().trim();
+    }
+
+    public String getCheckOutDate() {
+        // Returns the day text that was clicked (e.g., "14")
+        return clickCheckOut.getText().trim();
     }
 
 }
